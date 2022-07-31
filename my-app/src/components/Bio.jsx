e6ff5f364362537501b30707e4252622e1b0cec1
kailyl <kliu279@gmail.com>
2022-07-31 18:02:33 -0400

    changed major

--- my-app/src/components/Bio.jsx
+++ my-app/src/components/Bio.jsx
@@ -5,10 +5,9 @@ import Buttons from './Buttons';
 
 
 export default function Bio() {
-    const me = <a href="https://drive.google.com/file/d/1faPBd3xzovX8ovZOxSzwqMLz8kpybN3C/view?usp=sharing" className='link'>KAILY LIU</a>;
+    const me = <a href="https://drive.google.com/file/d/1RlMBHylw8dymkjUlQROeiMDD0jnY7lKO/view?usp=sharing" className='link'>KAILY LIU</a>;
     const penn = <a href="https://www.upenn.edu/" className='link2'>penn</a>;
-    const cs = <a href="https://www.cis.upenn.edu/undergraduate/program-offerings/computer-science-bse/" className='link2'>computer science</a>;
-    const econ = <a href="https://economics.sas.upenn.edu/undergraduate" className='link2'>economics</a>;
+    const nets = <a href="https://www.nets.upenn.edu/long/nets-curriculum" className='link2'>networked and social systems engineering</a>;
     const eent = <a href="https://eent.seas.upenn.edu/undergraduate-minor/" className='link2'>engineering entrepreneurship</a>;
     const math = <a href="https://www.math.upenn.edu/undergraduate/math-majors-and-minors/mathematics-minor" className='link2'>math</a>;
 
@@ -20,7 +19,7 @@ export default function Bio() {
                 <div> 
                     <div className='aboot'> 
                         <h1 className='myName'> {me}</h1>
-                        <p className='majors'> second year student at {penn} studying {cs} and {econ}. 
+                        <p className='majors'> second year student at {penn} studying {nets} (aka. classes in cs, economics, and systems engineering).
                         minors in {eent} and {math}.</p>
                     </div> 
                     <div className='iPt1'> 
--- my-app/src/components/Buttons.jsx
+++ my-app/src/components/Buttons.jsx
@@ -7,43 +7,43 @@ export default function Buttons() {
                 onClick={(e) => {
                 e.preventDefault(); 
                 window.location.href='https://drive.google.com/file/d/1faPBd3xzovX8ovZOxSzwqMLz8kpybN3C/view?usp=sharing';
-                }}> resume </button>
+                }}> resume &#128221;</button>
             <button 
                 className='buttonz'
                 onClick={(e) => {
                     e.preventDefault(); 
                     window.location.href='https://open.spotify.com/user/kailyliu';
-                }}> spotify </button>
+                }}> spotify &#127911;</button>
             <button 
                 className='buttonz' 
                 onClick={(e) => {
                 e.preventDefault(); 
                 window.location.href='https://www.linkedin.com/in/kaily-liu/';
-                }}> linkedin </button>
+                }}> linkedin &#128084;</button>
             <button 
                 className='buttonz' 
                 onClick={(e) => {
                     e.preventDefault(); 
                     window.location.href='mailto:kliu279@gmail.com';
-                }}> email </button>
-            <button 
-                className='buttonz'
-                onClick={(e) => {
-                    e.preventDefault(); 
-                    window.location.href='https://www.pinterest.com/kliu279/';
-                }}> pinterest </button>
+                }}> email &#128231;</button>
             <button 
                 className='buttonz' 
                 onClick={(e) => {
                 e.preventDefault(); 
                 window.location.href='https://github.com/kailyl';
-                }}> github </button>
+                }}> github &#128187;</button>
+            <button 
+                className='buttonz'
+                onClick={(e) => {
+                    e.preventDefault(); 
+                    window.location.href='https://www.pinterest.com/kliu279/';
+                }}> pinterest &#128444;</button>
             <button 
                 className='buttonz' 
                 onClick={(e) => {
                     e.preventDefault(); 
                     window.location.href='https://www.imdb.com/list/ls565032369/';
-                }}> imdb </button>
+                }}> imdb &#127909;</button>
         </div>
     )
 
